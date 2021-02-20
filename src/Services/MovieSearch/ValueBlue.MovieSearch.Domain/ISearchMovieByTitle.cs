5297b5aff86de5ab538992630f98d9a9f98ea51d
ilkertasatan <itasatan@gmail.com>
2021-02-20 21:51:49 +0100

    Introduce movie and its abstraction

---
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Domain/ISearchMovieByTitle.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ValueBlue.MovieSearch.Domain.Movies;
+
+namespace ValueBlue.MovieSearch.Domain
+{
+    public interface ISearchMovieByTitle
+    {
+        Task<Movie> GetMovieByTitleAsync(string title);
+    }
+}
