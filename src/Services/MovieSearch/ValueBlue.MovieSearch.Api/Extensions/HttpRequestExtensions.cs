7216d8d91f61fea10d8994133d8831c17fd08ac3
ilkertasatan <itasatan@gmail.com>
2021-02-21 17:55:03 +0100

    Add HttpRequest extensions

---
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Api/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ValueBlue.MovieSearch.Api.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string IpAddress(this HttpRequest request)
+        {
+            return request.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
