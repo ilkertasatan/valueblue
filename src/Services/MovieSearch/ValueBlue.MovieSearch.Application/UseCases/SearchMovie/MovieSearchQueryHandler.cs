5ff05f9085f86564900ec463c6b012aac7b01a27
ilkertasatan <itasatan@gmail.com>
2021-02-21 17:55:33 +0100

    Publish MovieSearchRequested domain event

--- src/Services/MovieSearch/ValueBlue.MovieSearch.Application/UseCases/SearchMovie/MovieSearchQueryHandler.cs
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Application/UseCases/SearchMovie/MovieSearchQueryHandler.cs
@@ -1,27 +1,45 @@
-﻿using System.Threading;
+﻿using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using ValueBlue.MovieSearch.Application.Common.Interfaces;
 using ValueBlue.MovieSearch.Domain;
+using ValueBlue.MovieSearch.Domain.Movies;
 
 namespace ValueBlue.MovieSearch.Application.UseCases.SearchMovie
 {
-    public class MovieSearchQueryHandler : 
+    public class MovieSearchQueryHandler :
         IRequestHandler<MovieSearchQuery, IQueryResult>
     {
+        private readonly IMediator _mediator;
         private readonly ISearchMovieByTitle _movieService;
 
-        public MovieSearchQueryHandler(ISearchMovieByTitle movieService)
+        public MovieSearchQueryHandler(
+            IMediator mediator,
+            ISearchMovieByTitle movieService)
         {
+            _mediator = mediator;
             _movieService = movieService;
         }
 
         public async Task<IQueryResult> Handle(MovieSearchQuery request, CancellationToken cancellationToken)
         {
-            var movie = await _movieService.GetMovieByTitleAsync(request.Title);
+            var stopwatch = Stopwatch.StartNew();
+            var movie = await _movieService.GetMovieByTitleAsync(request.MovieTitle);
+            stopwatch.Stop();
+
             if (!movie.Exists())
-                return new MovieNotFoundResult($"Movie '{movie.Title}' not found!");
-            
+                return new MovieNotFoundResult($"Movie '{request.MovieTitle}' not found!");
+
+            var since = stopwatch.ElapsedMilliseconds;
+            await _mediator.Publish(
+                new MovieSearchRequested(
+                    movie.Info.Title,
+                    movie.Imdb.ImdbId,
+                    since,
+                    request.IpAddress),
+                cancellationToken);
+
             return new MovieSearchSuccessResult(movie);
         }
     }
