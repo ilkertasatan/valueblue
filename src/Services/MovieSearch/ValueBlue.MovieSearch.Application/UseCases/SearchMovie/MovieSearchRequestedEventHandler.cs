5038b259f78ec053d28e910d49677f357a604b4f
ilkertasatan <itasatan@gmail.com>
2021-02-21 17:51:26 +0100

    Add MovieSearchRequested event handler

---
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Application/UseCases/SearchMovie/MovieSearchRequestedEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ValueBlue.MovieSearch.Domain;
+using ValueBlue.MovieSearch.Domain.Movies;
+using ValueBlue.MovieSearch.Infrastructure.DataAccess.Entities;
+
+namespace ValueBlue.MovieSearch.Application.UseCases.SearchMovie
+{
+    public class MovieSearchRequestedEventHandler :
+        INotificationHandler<MovieSearchRequested>
+    {
+        private readonly IRepository<MovieSearchRequest> _repository;
+
+        public MovieSearchRequestedEventHandler(
+            IRepository<MovieSearchRequest> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(MovieSearchRequested notification, CancellationToken cancellationToken)
+        {
+            var movieRequest = new MovieSearchRequestEntity(
+                notification.SearchToken,
+                notification.ImdbId,
+                notification.ProcessingTime,
+                notification.Timestamp,
+                notification.IpAddress);
+
+            _repository.InsertOneAsync(movieRequest, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Infrastructure/DataAccess/Entities/MovieSearchRequestEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ValueBlue.MovieSearch.Domain.Movies;
+
+namespace ValueBlue.MovieSearch.Infrastructure.DataAccess.Entities
+{
+    public sealed class MovieSearchRequestEntity :
+        MovieSearchRequest
+    {
+        private MovieSearchRequestEntity()
+        {
+        }
+        
+        public MovieSearchRequestEntity(
+            string searchToken,
+            string imdbId,
+            long processingTime,
+            DateTime timestamp,
+            string ipAddress)
+        {
+            Id = ObjectId.GenerateNewId();
+            SearchToken = searchToken;
+            ImdbId = imdbId;
+            ProcessingTime = processingTime;
+            Timestamp = timestamp;
+            IpAddress = ipAddress;
+        }
+        
+        public override object Id { get; }
+
+        [BsonElement("search_token")]
+        public override string SearchToken { get; }
+        
+        [BsonElement("imdbID")]
+        public override string ImdbId { get; }
+        
+        [BsonElement("processing_time_ms")]
+        public override long ProcessingTime { get; }
+        
+        [BsonElement("timestamp")]
+        public override DateTime Timestamp { get; }
+        
+        [BsonElement("ip_address")]
+        public override string IpAddress { get; }
+    }
+}
