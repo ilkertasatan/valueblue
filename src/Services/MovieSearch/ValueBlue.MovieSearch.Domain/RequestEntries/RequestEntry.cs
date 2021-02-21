4b2750e7ea346bb2d4ed868a90d14a1f76c4c373
ilkertasatan <itasatan@gmail.com>
2021-02-21 22:15:50 +0100

    Refactor: RequestEntry

--- src/Services/MovieSearch/ValueBlue.MovieSearch.Domain/RequestEntries/RequestEntry.cs
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Domain/RequestEntries/RequestEntry.cs
@@ -2,20 +2,41 @@
 
 namespace ValueBlue.MovieSearch.Domain.RequestEntries
 {
-    public abstract class RequestEntry :
-        IAggregate<object>,
+    public class RequestEntry :
+        IAggregate<Guid>,
         IMaybeExist
     {
-        public abstract object Id { get; protected set; }
-        public abstract string SearchToken { get; protected set; }
-        public abstract string ImdbId { get; protected set; }
-        public abstract long ProcessingTime { get; protected set; }
-        public abstract DateTime Timestamp { get; protected set; }
-        public abstract string IpAddress { get; protected set; }
+        public static readonly RequestEntry Empty = new RequestEntry();
+
+        public RequestEntry()
+        {
+        }
+
+        public RequestEntry(
+            string searchToken,
+            string imdbId,
+            long processingTime,
+            DateTime timestamp,
+            string ipAddress)
+        {
+            Id = Guid.NewGuid();
+            SearchToken = searchToken;
+            ImdbId = imdbId;
+            ProcessingTime = processingTime;
+            Timestamp = timestamp;
+            IpAddress = ipAddress;
+        }
+
+        public Guid Id { get; private set; }
+        public string SearchToken { get; private set; }
+        public string ImdbId { get; private set; }
+        public long ProcessingTime { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public string IpAddress { get; private set; }
 
         public bool Exists()
         {
-            return !Equals(Id, default);
+            return !Equals(Id, Guid.Empty);
         }
     }
 }
