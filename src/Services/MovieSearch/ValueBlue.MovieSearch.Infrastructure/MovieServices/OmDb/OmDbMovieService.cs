228bc602c266e5caeefa7c6082a06326351f3fdb
ilkertasatan <itasatan@gmail.com>
2021-02-21 17:53:23 +0100

    Integrate MovieTranslator

--- src/Services/MovieSearch/ValueBlue.MovieSearch.Infrastructure/MovieServices/OmDb/OmDbMovieService.cs
+++ src/Services/MovieSearch/ValueBlue.MovieSearch.Infrastructure/MovieServices/OmDb/OmDbMovieService.cs
@@ -12,17 +12,22 @@ namespace ValueBlue.MovieSearch.Infrastructure.MovieServices.OmDb
     {
         private readonly Uri _uri;
         private readonly string _apiKey;
+        private readonly ITranslateMovie<OmDbMovieResponse> _translator;
 
-        public OmDbMovieService(Uri uri, string apiKey)
+        public OmDbMovieService(
+            Uri uri,
+            string apiKey,
+            ITranslateMovie<OmDbMovieResponse> translator)
         {
             if (uri == null)
                 throw new ArgumentNullException(nameof(uri));
-            
+
             if (string.IsNullOrWhiteSpace(apiKey))
                 throw new ArgumentNullException(nameof(apiKey));
 
             _uri = uri;
             _apiKey = apiKey;
+            _translator = translator;
         }
 
         public async Task<Movie> GetMovieByTitleAsync(string title)
@@ -31,14 +36,16 @@ namespace ValueBlue.MovieSearch.Infrastructure.MovieServices.OmDb
 
             try
             {
-                movie = await _uri
+                var response = await _uri
                     .SetQueryParams(new
                     {
                         t = title,
                         apikey = _apiKey
                     })
                     .WithTimeout(TimeSpan.FromSeconds(3))
-                    .GetJsonAsync<Movie>();
+                    .GetJsonAsync<OmDbMovieResponse>();
+
+                movie = _translator.Translate(response);
             }
             catch (FlurlHttpTimeoutException)
             {
