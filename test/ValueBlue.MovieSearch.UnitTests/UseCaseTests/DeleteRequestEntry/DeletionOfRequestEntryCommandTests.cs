d774412c2a75a3c5bec799925887e78b739de465
ilkertasatan <itasatan@gmail.com>
2021-02-22 10:14:26 +0100

    Add DeleteRequestEntry tests

---
+++ test/ValueBlue.MovieSearch.UnitTests/UseCaseTests/DeleteRequestEntry/DeletionOfRequestEntryCommandTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using ValueBlue.MovieSearch.Application.Common.Model;
+using ValueBlue.MovieSearch.Application.UseCases.DeleteRequestEntry;
+using ValueBlue.MovieSearch.Domain;
+using ValueBlue.MovieSearch.Domain.RequestEntries;
+using Xunit;
+
+namespace ValueBlue.MovieSearch.UnitTests.UseCaseTests.DeleteRequestEntry
+{
+    public class DeletionOfRequestEntryCommandTests
+    {
+        [Fact]
+        public async Task Should_Delete_Request_Entry()
+        {
+            var repositoryMock = new Mock<IRepository<RequestEntry>>();
+            repositoryMock
+                .Setup(x => x.FindOneAsync(
+                    It.IsAny<Expression<Func<RequestEntry, bool>>>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new RequestEntry("search-token", "imdbId", 100, DateTime.Now, "ip_address"));
+            repositoryMock
+                .Setup(x => x.DeleteOneAsync(
+                    It.IsAny<Expression<Func<RequestEntry, bool>>>(),
+                    It.IsAny<CancellationToken>()));
+            var sut = new DeletionOfRequestEntryCommandHandler(repositoryMock.Object);
+
+            var actualResult = await sut.Handle(new DeletionOfRequestEntryCommand(Guid.NewGuid()), CancellationToken.None);
+
+            actualResult.Should().BeOfType<DeletionOfRequestEntrySuccessResult>();
+        }
+
+        [Fact]
+        public async Task Should_Not_Delete_Request_Entry_When_Request_Entry_Is_Not_Found()
+        {
+            var repositoryMock = new Mock<IRepository<RequestEntry>>();
+            repositoryMock
+                .Setup(x => x.FindOneAsync(
+                    It.IsAny<Expression<Func<RequestEntry, bool>>>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(RequestEntry.Empty);
+            repositoryMock
+                .Setup(x => x.DeleteOneAsync(
+                    It.IsAny<Expression<Func<RequestEntry, bool>>>(),
+                    It.IsAny<CancellationToken>()));
+            var sut = new DeletionOfRequestEntryCommandHandler(repositoryMock.Object);
+
+            var actualResult = await sut.Handle(new DeletionOfRequestEntryCommand(Guid.NewGuid()), CancellationToken.None);
+
+            actualResult.Should().BeOfType<RequestEntryNotFoundResult>();
+        }
+    }
+}
---
+++ test/ValueBlue.MovieSearch.UnitTests/UseCaseTests/DeleteRequestEntry/RequestEntryControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ValueBlue.MovieSearch.Api.UseCases.V1.DeleteRequestEntry;
+using ValueBlue.MovieSearch.Application.UseCases.DeleteRequestEntry;
+using Xunit;
+
+namespace ValueBlue.MovieSearch.UnitTests.UseCaseTests.DeleteRequestEntry
+{
+    public class RequestEntryControllerTests
+    {
+        [Fact]
+        public async Task Should_Return_204_When_Request_Entry_Is_Deleted()
+        {
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock
+                .Setup(x => x.Send(It.IsAny<DeletionOfRequestEntryCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new DeletionOfRequestEntrySuccessResult());
+            var sut = new RequestEntryController(mediatorMock.Object);
+
+            var actualResult = await sut.DeleteRequestEntryAsync(Guid.NewGuid());
+
+            actualResult.Should().BeOfType<NoContentResult>();
+        }
+    }
+}
