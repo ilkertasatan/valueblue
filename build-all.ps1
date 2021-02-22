9ea4734d63091061a12b4e0437da543944123690
ilkertasatan <itasatan@gmail.com>
2021-02-22 17:23:07 +0100

    Add docker support and ps script

---
+++ build-all.ps1
@@ -0,0 +1,7 @@
+﻿Write-host "Building process started..."  -ForegroundColor DarkGreen
+
+docker-compose down
+docker-compose build --no-cache
+docker-compose up -d
+
+Write-host "Building process completed."  -ForegroundColor DarkGreen
